29639e96dda31799e126024f1edc4a4d379a1e37
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-01-13 20:16:06 -0500

    added drama text

--- grails-app/domain/com/jvmdramas/app/Drama.groovy
+++ grails-app/domain/com/jvmdramas/app/Drama.groovy
@@ -3,9 +3,14 @@ package com.jvmdramas.app
 class Drama {
     String title
     String url
+    String drama
+    
     Date dateCreated
     Date lastUpdated
 
     static constraints = {
+    }
+    static mapping = {
+        drama type: 'text'
     }
 }
