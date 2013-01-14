23ed12fca478f98b95c17a7e5192dd2241fbf4d5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-01-13 20:51:32 -0500

    autostamping constraints

--- grails-app/domain/com/jvmdramas/app/Drama.groovy
+++ grails-app/domain/com/jvmdramas/app/Drama.groovy
@@ -14,6 +14,8 @@ class Drama {
     url blank: false, maxSize: 500, url: true
     drama blank: false
     status nullable: false
+    dateCreated nullable: true, display: false
+    lastUpdated nullable: true, display: false
   }
   static mapping = {
     drama type: 'text'
