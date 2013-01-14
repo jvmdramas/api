8eca9781e19f09e23dda7d2663175ffc9d920557
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-01-13 20:46:50 -0500

    added DramaStatus

--- grails-app/domain/com/jvmdramas/app/Drama.groovy
+++ grails-app/domain/com/jvmdramas/app/Drama.groovy
@@ -1,19 +1,21 @@
 package com.jvmdramas.app
 
 class Drama {
-    String title
-    String url
-    String drama
-    
-    Date dateCreated
-    Date lastUpdated
+  String title
+  String url
+  String drama
+  DramaStatus status = DramaStatus.PENDING
 
-    static constraints = {
-      title blank: false, maxSize: 200
-      url blank: false, maxSize: 500, url: true
-      drama blank: false
-    }
-    static mapping = {
-        drama type: 'text'
-    }
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+    title blank: false, maxSize: 200
+    url blank: false, maxSize: 500, url: true
+    drama blank: false
+    status nullable: false
+  }
+  static mapping = {
+    drama type: 'text'
+  }
 }
---
+++ src/groovy/com/jvmdramas/app/DramaStatus.groovy
@@ -0,0 +1,13 @@
+package com.jvmdramas.app
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: domix
+ * Date: 13/01/13
+ * Time: 20:45
+ * To change this template use File | Settings | File Templates.
+ */
+enum DramaStatus {
+  PENDING,
+  APPROVED
+}
