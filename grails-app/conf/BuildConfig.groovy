b63030c32a3684ccfb93038386940507cb104654
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-01-27 00:35:18 -0500

    improved BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,72 +1,53 @@
-grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/${appName}.war"
+grails.work.dir = 'target'
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+        run: [maxMemory: 1024, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 grails.project.dependency.resolution = {
-  // inherit Grails' default dependencies
-  inherits("global") {
-    // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-    // excludes 'ehcache'
-  }
-  log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-  checksums true // Whether to verify checksums on resolve
-  legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+        // excludes 'ehcache'
+    }
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-  repositories {
-    inherits true // Whether to inherit repository definitions from plugins
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
 
-    grailsPlugins()
-    grailsHome()
-    grailsCentral()
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
 
-    mavenLocal()
-    mavenCentral()
+        mavenLocal()
+        mavenCentral()
 
-    // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-    //mavenRepo "http://snapshots.repository.codehaus.org"
-    //mavenRepo "http://repository.codehaus.org"
-    //mavenRepo "http://download.java.net/maven/2/"
-    //mavenRepo "http://repository.jboss.com/maven2/"
-  }
+    }
 
-  dependencies {
-    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+    dependencies {
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        test "org.gebish:geb-spock:0.9.0-RC-1"
+    }
 
-    // runtime 'mysql:mysql-connector-java:5.1.20'
-  }
-
-  plugins {
-    runtime ":hibernate:$grailsVersion"
-    runtime ":jquery:1.8.3"
-    runtime ":resources:1.2.RC2"
-
-    // Uncomment these (or add new ones) to enable additional resources capabilities
-    //runtime ":zipped-resources:1.0"
-    //runtime ":cached-resources:1.0"
-    //runtime ":yui-minify-resources:0.1.4"
-
-    build ":tomcat:$grailsVersion"
-
-    runtime ':database-migration:1.2.2'
-
-    compile ':cache:1.0.1'
-    compile ':joda-time:1.4'
-
-    //test ':code-coverage:1.2.5'
-  }
-}
-
-coverage {
-  exclusions = [
-    'ApplicationResources**',
-    'GrailsMelodyConfig**']
+    plugins {
+        build ":tomcat:$grailsVersion"
+        compile ':cache:1.0.1'
+        compile ":spring-security-core:1.2.7.3"
+        compile ":mongodb:1.1.0.GA"
+        compile ":codenarc:0.18"
+        test ":geb:0.9.0-RC-1"
+        test(":spock:0.7") {
+            exclude "spock-grails-support"
+        }
+    }
 }
